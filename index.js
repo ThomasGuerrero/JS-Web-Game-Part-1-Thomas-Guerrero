b4330a02e1461fe6258c6b19105754c1d858ed13
Thomas Guerrero <thomaseguerrero@gmail.com>
2023-03-09 15:45:41 -0500

    added code to js

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (image, left, bottom) {
+    let item = document.createElement('img')
+    item.src = image
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+    return item
+}
+
+function newItem (image, left, bottom) {
+    let item = newImage(image, left, bottom)
+    item.addEventListener('dblclick', () => {
+        item.remove() 
+    })
+}
+
+newImage ('assets/green-character.gif', '100', '100')
+newImage ('assets/pine-tree.png', '450', '200')
+newImage ('assets/well.png', '300', '300')
+newImage ('assets/tree.png', '700', '400')
+
+newItem ('assets/sheild.png', '165', '185')
+newItem ('assets/staff.png', '600', '100')
+newItem ('assets/sword.png','300', '100')
+
